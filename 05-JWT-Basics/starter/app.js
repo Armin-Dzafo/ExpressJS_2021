f24402ab0664df228fd45dff100eca160fe0ce42
alojsb <alojsbirmingham@gmail.com>
2021-12-25 23:58:52 +0100

    proper formatting (eslint)

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 
 const express = require('express');
+
 const app = express();
 
 const notFoundMiddleware = require('./middleware/not-found');
@@ -17,13 +18,11 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -1,9 +1,11 @@
-const CustomAPIError = require('../errors/custom-error')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).send('Something went wrong try again later')
-}
+const CustomAPIError = require('../errors/custom-error');
 
-module.exports = errorHandlerMiddleware
+// eslint-disable-next-line no-unused-vars
+const errorHandlerMiddleware = (err, req, res, next) => {
+    if (err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({ msg: err.message });
+    }
+    return res.status(500).send('Something went wrong try again later');
+};
+
+module.exports = errorHandlerMiddleware;
